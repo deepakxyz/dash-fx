906ef9f909a3fa53d63c63fedef3043dc82b4282
Deepak <deepakrajan.grea@gmail.com>
2021-03-30 01:58:21 +0530

    foundry beforeandafter fxguide foundryinsight fetch done

--- dash-server/data_scrape.js
+++ dash-server/data_scrape.js
@@ -36,14 +36,24 @@ const get_sub_push = (fetchData,  filename,firstElementName,url) => {
 
 
 const artofvfx = () => {
-    url = "https://www.artofvfx.com/category/interviews/"
-    axios.get(url)
+    site_url = "https://www.artofvfx.com/category/interviews/"
+    axios.get(site_url)
         .then((res)=>{
             // load website data
             const $ = cheerio.load(res.data);
 
             // empty fecth data array
             const fetched_data = [];
+
+            // get the top two data
+            // $('.td_module_mx5').each((index,element)=>{
+            //     // get the url of the article
+            //     var title = $(element).find('div.td-meta-info-container').find('a').text();
+            //     var url = $(element).find('div.td-meta-info-container').find('a').attr('href');
+            //     var imgUrl = $(element).find('div.td-module-thumb').children().children().attr('data-img-url');
+
+            //     console.log(imgUrl);
+            // })
 
 
             // get the required data and push it into an array
@@ -55,27 +65,30 @@ const artofvfx = () => {
                 // get image of the article
                 const imgUrl = $(element).find('img.entry-thumb').attr('data-img-url');
                 // get sub title data
-                var subTitle = $(element).find('div.td-excerpt').text();
+                var description = $(element).find('div.td-excerpt').text();
                 // remove all the special characters
-                subTitle = subTitle.replace(/(\r\n|\n|\r)/gm, "");
+                description = description.replace(/(\r\n|\n|\r)/gm, "");
                 // remove the extra white spaces
-                subTitle = subTitle.trim();
+                description = description.trim();
                 // create new date
+                var type = "Interview";
                 var date = new Date();
                 // push it into the articles array
-                fetched_data[index] = {title,url,imgUrl,subTitle,date};
+                fetched_data[index] = {title,url,imgUrl,description,type,date};
             });
 
-            get_sub_push(fetched_data,"artofvfx.json","title",url)
+            get_sub_push(fetched_data,"artofvfx.json","title",site_url)
 
         })
         .catch(err=>console.error(err.message));
 }
 
+artofvfx()
+
 // beforeandafter
 const beforeandafter = () => {
-    let url = "https://beforesandafters.com/";
-    axios.get(url)
+    let site_url = "https://beforesandafters.com/";
+    axios.get(site_url)
         .then((res)=>{
             // load website data
             const $ = cheerio.load(res.data);
@@ -93,25 +106,28 @@ const beforeandafter = () => {
                 // get the image of the article
                 const imgUrl =  $(element).children().find('img.attachment-patch-masonry-image').attr('src');
                 // get sub title
-                const subTitle = $(element).find('div.entry-content').children().text()
+                const description = $(element).find('div.entry-content').children().text()
+                // type
+                var type = "Article";
                 // create new data
                 var date = new Date();
                 // push it into the article array
-                fetched_data[index] = {title,url,imgUrl,subTitle,date};
+                fetched_data[index] = {title,url,imgUrl,description,type,date};
             });
 
             // var data = JSON.stringify(fetched_data,null, 4);
             // fs.writeFile("beforeandafter.json",data,msg=>console.log("Done fetching from: " + "beforeandafter"));
 
-            get_sub_push(fetched_data,"beforeandafter.json","title",url);
+            get_sub_push(fetched_data,"beforeandafter.json","title",site_url);
     })
 }
+
 
 // fxguide
 const fxguide = () => {
 
-    let url = "https://www.fxguide.com/";
-    axios.get(url)
+    let site_url = "https://www.fxguide.com/";
+    axios.get(site_url)
         .then((res)=>{
             // load website data
             const $ = cheerio.load(res.data);
@@ -130,18 +146,63 @@ const fxguide = () => {
                 // get the image of the article
                 const imgUrl = $(element).find('div.fl-post-grid-image').children().children().attr('src');
                 // get sub title
-                const subTitle = $(element).find('div.fl-post-grid-text').find('div.fl-post-grid-content').children().text()
+                const description = $(element).find('div.fl-post-grid-text').find('div.fl-post-grid-content').children().text()
+                // type
+                var type = "Article";
                 // create new date
                 var date = new Date();
                 // push it into the articles array
-                fetched_data[index] = {title,url,imgUrl,subTitle,date};
+                fetched_data[index] = {title,url,imgUrl,description,type,date};
             });
 
 
-                get_sub_push(fetched_data,"fxguide.json","title",url)
+                get_sub_push(fetched_data,"fxguide.json","title",site_url)
 
         })
         .catch(err=>console.error(err.message));
 }
 
+
 // foundry insights
+const foundryinsights = () => {
+    let site_url = "https://www.foundry.com/insights";
+    axios.get(site_url)
+        .then((res)=>{
+            // load website data
+            const $ = cheerio.load(res.data);
+
+            // empty fecth data array
+            const fetched_data = [];
+
+
+            // get the required data and push it into an array
+            $('.featured-card').each((index, element)=> {
+                // get title of the article
+                var title = $(element).children().find('h3.featured-card__title').html()
+                title = title.trim()
+                // get url of the artile
+                var url = $(element).children('a').attr('href');
+                url = 'https://www.foundry.com/' + url;
+                // get the image url
+                var imgUrl = $(element).find('div.featured-card__image-container').children('img').attr("src");
+                imgUrl = "https://www.foundry.com" + imgUrl;
+                // get description
+                var description = $(element).find('p.featured-card__description').text();
+                description = description.replace(/(\r\n|\n|\r)/gm, "");
+                description = description.trim()
+                // type of the content
+                var type = $(element).find('span.featured-card__tag').text()
+                // create new date
+                var date = new Date();
+                // push it into the articles array
+                fetched_data[index] = {title,url,imgUrl,description,type,date};
+            });
+
+
+                get_sub_push(fetched_data,"foundryinsights.json","title",site_url)
+
+        })
+        .catch(err=>console.error(err.message));
+}
+
+// foundryinsights()
